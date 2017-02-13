500279343795965605deceb3b0fbc66fea11aada
Jan Max Meyer <jmm@phorward.de>
2017-02-13 13:55:54 +0100

    Adding parseFloat() and parseInt() utility functions.

--- utils.py
+++ utils.py
@@ -65,3 +65,37 @@ def textToHtml(node, text):
 			node.appendChild(html5.Br())
 
 		node.appendChild(html5.TextNode(part))
+
+def parseInt(s, ret = 0):
+	"""
+	Parses a value as int
+	"""
+	if not isinstance(s, str):
+		return int(s)
+	elif s:
+		if s[0] in "+-":
+			ts = s[1:]
+		else:
+			ts = s
+
+		if ts and all([_ in "0123456789" for _ in ts]):
+			return int(s)
+
+	return ret
+
+def parseFloat(s, ret = 0.0):
+	"""
+	Parses a value as float.
+	"""
+	if not isinstance(s, str):
+		return float(s)
+	elif s:
+		if s[0] in "+-":
+			ts = s[1:]
+		else:
+			ts = s
+
+		if ts and ts.count(".") <= 1 and all([_ in ".0123456789" for _ in ts]):
+			return float(s)
+
+	return ret
