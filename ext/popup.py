b5933874ec799df9add5c12c63a73da70199cb85
Jan Max Meyer <jmm@phorward.de>
2018-05-22 17:04:11 +0200

    Started refactoring popup tools

--- ext/popup.py
+++ ext/popup.py
@@ -1,8 +1,9 @@
 import html5
 
-class Popup( html5.Div ):
-	def __init__(self, title=None, id=None, className=None, *args, **kwargs ):
-		super( Popup, self ).__init__(*args, **kwargs)
+
+class Popup(html5.Div):
+	def __init__(self, title=None, id=None, className=None, *args, **kwargs):
+		super(Popup, self).__init__(*args, **kwargs)
 
 		self["class"] = "alertbox"
 		if className is not None and len(className):
@@ -11,8 +12,8 @@ class Popup( html5.Div ):
 		if title:
 			lbl = html5.Span()
 			lbl["class"].append("title")
-			lbl.appendChild( html5.TextNode( title ) )
-			self.appendChild( lbl )
+			lbl.appendChild(html5.TextNode(title))
+			self.appendChild(lbl)
 
 		# id can be used to pass information to callbacks
 		self.id = id
@@ -20,17 +21,19 @@ class Popup( html5.Div ):
 		self.frameDiv = html5.Div()
 		self.frameDiv["class"] = "popup"
 
-		self.frameDiv.appendChild( self )
-		html5.Body().appendChild( self.frameDiv )
+		self.frameDiv.appendChild(self)
+		html5.Body().appendChild(self.frameDiv)
 
 	def close(self, *args, **kwargs):
-		html5.Body().removeChild( self.frameDiv )
+		html5.Body().removeChild(self.frameDiv)
 		self.frameDiv = None
+
 
 class Alert(Popup):
 	"""
 	Just displaying an alerting message box with OK-button.
 	"""
+
 	def __init__(self, msg, title=None, okCallback=None, okLabel="OK", *args, **kwargs):
 		super(Alert, self).__init__(title, *args, **kwargs)
 		self.addClass("alert")
@@ -76,9 +79,11 @@ class Alert(Popup):
 			event.preventDefault()
 			self.onOkBtnClick()
 
-class YesNoDialog( Popup ):
-	def __init__(self, question, title=None, yesCallback=None, noCallback=None, yesLabel="Yes", noLabel="No", *args, **kwargs):
-		super( YesNoDialog, self ).__init__( title, *args, **kwargs )
+
+class YesNoDialog(Popup):
+	def __init__(self, question, title=None, yesCallback=None, noCallback=None, yesLabel="Yes", noLabel="No", *args,
+	             **kwargs):
+		super(YesNoDialog, self).__init__(title, *args, **kwargs)
 		self["class"].append("yesnodialog")
 
 		self.yesCallback = yesCallback
@@ -93,12 +98,12 @@ class YesNoDialog( Popup ):
 		else:
 			html5.utils.textToHtml(lbl, question)
 
-		btnYes = html5.ext.Button(yesLabel, callback=self.onYesClicked )
+		btnYes = html5.ext.Button(yesLabel, callback=self.onYesClicked)
 		btnYes["class"].append("btn_yes")
 		self.appendChild(btnYes)
 
 		if len(noLabel):
-			btnNo = html5.ext.Button(noLabel, callback=self.onNoClicked )
+			btnNo = html5.ext.Button(noLabel, callback=self.onNoClicked)
 			btnNo["class"].append("btn_no")
 			self.appendChild(btnNo)
 
@@ -131,84 +136,103 @@ class YesNoDialog( Popup ):
 		self.noCallback = None
 		self.close()
 
-	def onYesClicked(self, *args, **kwargs ):
+	def onYesClicked(self, *args, **kwargs):
 		if self.yesCallback:
-			self.yesCallback( self )
+			self.yesCallback(self)
 
 		self.drop()
 
-	def onNoClicked(self, *args, **kwargs ):
+	def onNoClicked(self, *args, **kwargs):
 		if self.noCallback:
-			self.noCallback( self )
+			self.noCallback(self)
 
 		self.drop()
 
-class SelectDialog( Popup ):
 
-	def __init__( self, prompt, items=None, title=None, okBtn="OK", cancelBtn="Cancel", forceSelect=False, *args, **kwargs ):
-		super( SelectDialog, self ).__init__( title, *args, **kwargs )
+class SelectDialog(Popup):
+
+	def __init__(self, prompt, items=None, title=None, okBtn="OK", cancelBtn="Cancel", forceSelect=False,
+	                callback=None, *args, **kwargs):
+		super(SelectDialog, self).__init__(title, *args, **kwargs)
 		self["class"].append("selectdialog")
+
+		self.callback = callback
+		self.items = items
+		assert isinstance(self.items, list)
 
 		# Prompt
 		if prompt:
 			lbl = html5.Span()
-			lbl[ "class" ].append( "prompt" )
-			lbl.appendChild( html5.TextNode( prompt ) )
-			self.appendChild( lbl )
+			lbl["class"].append("prompt")
+			lbl.appendChild(html5.TextNode(prompt))
+			self.appendChild(lbl)
 
 		# Items
-		self.items = []
+		if not forceSelect and len(items) <= 3:
+			for idx, item in enumerate(items):
+				if isinstance(item, dict):
+					title = item.get("title")
+					cssc = item.get("class")
+				elif isinstance(item, tuple):
+					title = item[1]
+					cssc = None
+				else:
+					title = item
 
-		if not forceSelect and len( items ) <= 3:
-			for item in items:
-				btn = html5.ext.Button( item.get( "title" ), callback=self.onAnyBtnClick )
-				btn._callback = item.get( "callback" )
+				btn = html5.ext.Button(title, callback=self.onAnyBtnClick)
+				btn.idx = idx
 
-				if item.get( "class" ):
-					btn[ "class" ].extend( item[ "class" ] )
+				if cssc:
+					btn.addClass(cssc)
 
-				self.appendChild( btn )
-				self.items.append( btn )
+				self.appendChild(btn)
 		else:
 			self.select = html5.Select()
-			self.appendChild( self.select )
+			self.appendChild(self.select)
 
-			for i, item in enumerate( items ):
-				opt = html5.Option()
+			for idx, item in enumerate(items):
+				if isinstance(item, dict):
+					title = item.get("title")
+				elif isinstance(item, tuple):
+					title = item[1]
+				else:
+					title = item
 
-				opt[ "value" ] = str( i )
-				opt._callback = item.get( "callback" )
-				opt.appendChild( html5.TextNode( item.get( "title" ) ) )
+				opt = html5.Option(title)
+				opt["value"] = str(idx)
 
-				self.select.appendChild( opt )
-				self.items.append( opt )
+				self.select.appendChild(opt)
 
 			if okBtn:
-				self.appendChild( html5.ext.Button( okBtn, callback=self.onOkClick ) )
+				self.appendChild(html5.ext.Button(okBtn, callback=self.onOkClick))
 
 			if cancelBtn:
-				self.appendChild( html5.ext.Button( cancelBtn, callback=self.onCancelClick ) )
+				self.appendChild(html5.ext.Button(cancelBtn, callback=self.onCancelClick))
 
-	def onAnyBtnClick( self, sender = None ):
-		for btn in self.items:
-			if btn == sender:
-				if btn._callback:
-					btn._callback( self )
-				break
+	def onAnyBtnClick(self, sender):
+		item = self.items[sender.idx]
+
+		if isinstance(item, dict) and item.get("callback") and callable(item["callback"]):
+			item["callback"](item)
+
+		if self.callback:
+			self.callback(item)
 
 		self.items = None
 		self.close()
 
-	def onCancelClick(self, sender = None ):
+	def onCancelClick(self, sender=None):
 		self.close()
 
-	def onOkClick(self, sender = None ):
-		if self.select[ "selectedIndex" ] < 0:
-			return
+	def onOkClick(self, sender=None):
+		assert self.select["selectedIndex"] >= 0
+		item = self.item[int(self.select.children(self.select["selectedIndex"])["value"])]
 
-		item = self.items[ int( self.select[ "options" ].item( self.select[ "selectedIndex" ] ).value ) ]
-		if item._callback:
-			item._callback( self )
+		if isinstance(item, dict) and item.get("callback") and callable(item["callback"]):
+			item["callback"](item)
+
+		if self.callback:
+			self.callback(item)
 
 		self.items = None
 		self.select = None
